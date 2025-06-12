aa03125b6f2a19c376a1dd4687995827609bb709
limits1214 <limits1214@gmail.com>
2025-06-12 12:16:21 +0900

    The requested image's platform (linux/amd64) does not match the detected host platform (linux/arm64/v8) 대응

--- back/build_docker_api.bash
+++ back/build_docker_api.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker build . -t ghcr.io/limits1214/t2ris-back-api:latest -f api/Dockerfile
+docker build . -t ghcr.io/limits1214/t2ris-back-api:latest -f api/Dockerfile --platform linux/arm64
 docker push ghcr.io/limits1214/t2ris-back-api:latest
--- front/build_docker_front.bash
+++ front/build_docker_front.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker build . -t ghcr.io/limits1214/t2ris-front:latest
+docker build . -t ghcr.io/limits1214/t2ris-front:latest --platform linux/arm64
 docker push ghcr.io/limits1214/t2ris-front:latest

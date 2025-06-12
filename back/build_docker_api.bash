6f668f7ff6f0e2d4305fc74c43a843c1d54d8c1c
limits1214 <limits1214@gmail.com>
2025-06-12 12:24:07 +0900

    대응 이슈, docker build -> buildx 변경

--- back/build_docker_api.bash
+++ back/build_docker_api.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-docker build . -t ghcr.io/limits1214/t2ris-back-api:latest -f api/Dockerfile --platform linux/arm64
-docker push ghcr.io/limits1214/t2ris-back-api:latest
+docker buildx build \
+  --platform linux/arm64 \
+  -t ghcr.io/limits1214/t2ris-back-api:latest \
+  --push \
+  .
+
+# docker build . -t ghcr.io/limits1214/t2ris-back-api:latest -f api/Dockerfile --platform linux/arm64
+# docker push ghcr.io/limits1214/t2ris-back-api:latest
--- front/build_docker_front.bash
+++ front/build_docker_front.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-docker build . -t ghcr.io/limits1214/t2ris-front:latest --platform linux/arm64
-docker push ghcr.io/limits1214/t2ris-front:latest
+docker buildx build \
+  --platform linux/arm64 \
+  -t ghcr.io/limits1214/t2ris-front:latest \
+  --push \
+  .
+
+# docker build . -t ghcr.io/limits1214/t2ris-front:latest --platform linux/arm64
+# docker push ghcr.io/limits1214/t2ris-front:latest

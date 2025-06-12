d476b538b47abd27182151d97f16b69c759df46c
limits1214 <limits1214@gmail.com>
2025-06-13 00:59:30 +0900

    action ubuntu-22.04 -> ubuntu-22.04-arm, qemu + buildx 제거

--- back/build_docker_api.bash
+++ back/build_docker_api.bash
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
-docker buildx build \
+docker build \
   -f api/Dockerfile \
-  --platform linux/arm64 \
   -t ghcr.io/limits1214/t2ris-back-api:latest \
   --push \
   .
+
+# docker buildx build \
+#   -f api/Dockerfile \
+#   --platform linux/arm64 \
+#   -t ghcr.io/limits1214/t2ris-back-api:latest \
+#   --push \
+#   .
 
 # docker build . -t ghcr.io/limits1214/t2ris-back-api:latest -f api/Dockerfile --platform linux/arm64
 # docker push ghcr.io/limits1214/t2ris-back-api:latest
--- front/build_docker_front.bash
+++ front/build_docker_front.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-docker buildx build \
-  --platform linux/arm64 \
+docker build \
   -t ghcr.io/limits1214/t2ris-front:latest \
   --push \
   .
+
+# docker buildx build \
+#   --platform linux/arm64 \
+#   -t ghcr.io/limits1214/t2ris-front:latest \
+#   --push \
+#   .
 
 # docker build . -t ghcr.io/limits1214/t2ris-front:latest --platform linux/arm64
 # docker push ghcr.io/limits1214/t2ris-front:latest
